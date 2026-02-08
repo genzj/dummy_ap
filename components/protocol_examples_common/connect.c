a3d55b6a07208ae4368decbeed8ca51296e4ac95
genzj <zj0512@gmail.com>
2026-02-08 10:38:52 -0800

    Remove unnecessary includes from connect.c and update CMakeLists.txt to reflect changes in component dependencies.

--- components/protocol_examples_common/connect.c
+++ components/protocol_examples_common/connect.c
@@ -7,10 +7,6 @@
 #include <string.h>
 #include "protocol_examples_common.h"
 #include "example_common_private.h"
-#include "sdkconfig.h"
-#include "esp_event.h"
-#include "esp_wifi.h"
-#include "esp_wifi_default.h"
 #include "esp_log.h"
 #include "esp_netif.h"
 #include "freertos/FreeRTOS.h"
